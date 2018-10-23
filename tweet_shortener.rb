fd2074247f76d1213de6519a292d795911ca24aa
Jack Ling <j.lingyx@gmail.com>
2018-10-23 20:43:33 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,6 +39,8 @@ end
 def shortened_tweet_truncator(string)
     if string.length < 140
     return string
+    elsif word_substituter(string).length < 140
+    return word_substituter(string)
   elsif word_substituter(string).length > 140
     return word_substituter(string)[0...140]
 end
