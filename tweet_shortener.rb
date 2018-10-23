c9a966479c27f1462bce16012ab73997c30bbf1f
Jack Ling <j.lingyx@gmail.com>
2018-10-23 19:44:24 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,7 +16,7 @@ end
 def word_substituter(string)
   array = string.split(" ")
   subs = dictionary.keys
-  array.map do |word| 
+  array.each do |word| 
     if subs.include?(word)
       
     end
